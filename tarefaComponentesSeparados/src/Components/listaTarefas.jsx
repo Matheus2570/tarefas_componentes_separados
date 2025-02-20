1630a555ad0c09d8b4d3013ffce5dab7c5d69a5b
Maria Clara Freire <mariaclaracgfreire@gmail.com>
2025-02-20 09:35:58 -0300

    Dia 20/02/2025

---
+++ tarefaComponentesSeparados/src/Components/listaTarefas.jsx
@@ -0,0 +1,49 @@
+import tarefas from "./Components/tarefas"
+function tarefas(){
+return(
+<>
+
+<tarefas
+tarefaRealizada="- Arrumar o quarto."
+/>
+
+<tarefas
+tarefaRealizada="- Fazer a pesquisa de Geografia."
+/>
+
+<tarefas
+tarefaRealizada="- Fazer o poema de Português."
+/>
+
+<tarefas
+tarefaRealizada="- Organizar o Diário de bordo de Matemática."
+/>
+
+<tarefas
+tarefaRealizada="- Tirar o lixo."
+/>
+
+<tarefas
+tarefaRealizada="- Passear com o Tobby Johnson Silva Oliveira de vovó, as 14:30."
+/>
+
+<tarefas
+tarefaRealizada="- Ir na academia."
+/>
+
+<tarefas
+tarefaRealizada="- Terminar Game of Thrones."
+/>
+
+<tarefas
+tarefaRealizada="- Fazer almoço(Strogonoff)."
+/>
+
+<tarefas
+tarefaRealizada="- Praticar inglês."
+/>
+
+</>
+)
+
+}
