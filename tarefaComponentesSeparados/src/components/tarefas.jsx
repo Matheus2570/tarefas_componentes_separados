62cd82b5730b795a0bf205e52469c80b5a39bdec
Matheus de Carvalho <matheusdecarvalho100@gmail.com>
2025-02-20 09:33:50 -0300

    Dia 20/02/2025

---
+++ tarefaComponentesSeparados/src/components/tarefas.jsx
@@ -0,0 +1,33 @@
+import "./tarefas.css";
+import { useState } from "react";
+
+function Tarefas(props) {
+  const [Concluido, setConcluindo] = useState(false);
+
+  const Concluir = () => {
+    setConcluindo(true);
+  };
+
+  return (
+    <>
+    <div className="blocoMaior">
+    <div className="blocoTarefas">
+        <p
+          className={`tarefaRealizada ${Concluido ? "risco" : ""}`}
+        >
+    {props.tarefaRealizada}
+        </p>
+        <button
+          onClick={Concluir}
+          className={Concluido ? "Concluida" : "botaoConcluir"}
+          disabled={Concluido}
+        >
+          {Concluido ? "Concluida" : "Concluir"}
+        </button>
+      </div>
+    </div>
+    </>
+  );
+}
+
+export default Tarefas;
